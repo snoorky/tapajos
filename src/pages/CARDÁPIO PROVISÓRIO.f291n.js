0a3bbcadb28bfd4d8034e8da0a430344d694b354
Raphael Vitor <raphaelvitorlopes@icloud.com>
2024-04-19 11:50:25 -0300

    fix: ajuste cardapio

--- src/pages/CARDÁPIO PROVISÓRIO.f291n.js
+++ src/pages/CARDÁPIO PROVISÓRIO.f291n.js
@@ -1,88 +1,67 @@
 import wixWindowFrontend from 'wix-window-frontend';
 
 $w.onReady(function () {
-	var petiscos = $w('#box2');
-	var bolinhos = $w('#box3');
-	var carnes = $w('#box4');
-	var principal = $w('#box5');
-	var sobremesa = $w('#box6');
-	var acompanhamento = $w('#box7');
-	var doses = $w('#box8');
-	var chops = $w('#box9');
-	var bebidas = $w('#box10');
+	var petiscos = $w('#sectionPetiscos');
+	var carnes = $w('#sectioncarnes');
+	var principal = $w('#sectionPrincipal');
+	var sobremesa = $w('#sectionSobremesa');
+	var acompanhamento = $w('#sectionAcompanhamento');
+	var bebidas = $w('#sectionbebidas');
 	var dropdown = $w('#dropFilters');
 
 	if (wixWindowFrontend.formFactor === "Desktop") {
-		$w('#btnPrincipal').onClick(function() {
-			principal.expand();
-			carnes.collapse();
-			petiscos.collapse();
-			bolinhos.collapse();
-			sobremesa.collapse();
-			doses.collapse();
-			chops.collapse();
-			bebidas.collapse();
-			acompanhamento.collapse();
+		petiscos.onClick(function () {
+			petiscos.show();
+			carnes.hide();
+			principal.hide();
+			sobremesa.hide();
+			acompanhamento.hide();
+			bebidas.hide();
 		});
 
-		$w('#btnCarnes').onClick(function() {
-			principal.collapse();
-			carnes.expand();
-			petiscos.collapse();
-			bolinhos.collapse();
-			sobremesa.collapse();
-			doses.collapse();
-			chops.collapse();
-			bebidas.collapse();
-			acompanhamento.collapse();
+		carnes.onClick(function () {
+			petiscos.hide();
+			carnes.show();
+			principal.hide();
+			sobremesa.hide();
+			acompanhamento.hide();
+			bebidas.hide();
 		});
 
-		$w('#btnPetiscos').onClick(function() {
-			principal.collapse();
-			carnes.collapse();
-			petiscos.expand();
-			bolinhos.expand();
-			sobremesa.collapse();
-			doses.collapse();
-			chops.collapse();
-			bebidas.collapse();
-			acompanhamento.collapse();
+		principal.onClick(function () {
+			petiscos.hide();
+			carnes.hide();
+			principal.show();
+			sobremesa.hide();
+			acompanhamento.hide();
+			bebidas.hide();
 		});
 
-		$w('#btnSobremesa').onClick(function() {
-			principal.collapse();
-			carnes.collapse();
-			petiscos.collapse();
-			bolinhos.collapse();
-			sobremesa.expand();
-			doses.collapse();
-			chops.collapse();
-			bebidas.collapse();
-			acompanhamento.collapse();
+		sobremesa.onClick(function () {
+			petiscos.hide();
+			carnes.hide();
+			principal.hide();
+			sobremesa.show();
+			acompanhamento.hide();
+			bebidas.hide();
 		});
 
-		$w('#btnBebidas').onClick(function() {
-			principal.collapse();
-			carnes.collapse();
-			petiscos.collapse();
-			bolinhos.collapse();
-			sobremesa.collapse();
-			doses.expand();
-			chops.expand();
-			bebidas.expand();
-			acompanhamento.collapse();
+		acompanhamento.onClick(function () {
+			petiscos.hide();
+			carnes.hide();
+			principal.hide();
+			sobremesa.hide();
+			acompanhamento.show();
+			bebidas.hide();
 		});
 
-		$w('#btnAcompanhamentos').onClick(function() {
-			principal.collapse();
-			carnes.collapse();
-			petiscos.collapse();
-			bolinhos.collapse();
-			sobremesa.collapse();
-			doses.collapse();
-			chops.collapse();
-			bebidas.collapse();
-			acompanhamento.expand();
+		bebidas.onClick(function () {
+			petiscos.hide();
+			carnes.hide();
+			principal.hide();
+			sobremesa.hide();
+			acompanhamento.hide();
+			bebidas.show();
 		});
 	};
 
@@ -90,85 +69,67 @@ $w.onReady(function () {
 		dropdown.expand();
 
 		dropdown.options = [
-			{"label": "Petiscos", "value": "Petiscos"},
-			{"label": "Carnes", "value": "Carnes"},
-			{"label": "Principal", "value": "Principal"},
-			{"label": "Sobremesa", "value": "Sobremesa"},
-			{"label": "Acompanhamentos", "value": "Acompanhamentos"},
-			{"label": "Bebidas", "value": "Bebidas"}
+			{ "label": "Petiscos", "value": "Petiscos" },
+			{ "label": "Carnes", "value": "Carnes" },
+			{ "label": "Principal", "value": "Principal" },
+			{ "label": "Sobremesa", "value": "Sobremesa" },
+			{ "label": "Acompanhamentos", "value": "Acompanhamentos" },
+			{ "label": "Bebidas", "value": "Bebidas" }
 		]
 
 		dropdown.onChange((event) => {
 			switch (event.target.value) {
 				case "Principal":
-					principal.expand();
-					carnes.collapse();
-					petiscos.collapse();
-					bolinhos.collapse();
-					sobremesa.collapse();
-					doses.collapse();
-					chops.collapse();
-					bebidas.collapse();
-					acompanhamento.collapse();
+					petiscos.hide();
+					carnes.hide();
+					principal.show();
+					sobremesa.hide();
+					acompanhamento.hide();
+					bebidas.hide();
 					break;
 				case "Carnes":
-					principal.collapse();
-					carnes.expand();
-					petiscos.collapse();
-					bolinhos.collapse();
-					sobremesa.collapse();
-					doses.collapse();
-					chops.collapse();
-					bebidas.collapse();
-					acompanhamento.collapse();
+					petiscos.hide();
+					carnes.show();
+					principal.hide();
+					sobremesa.hide();
+					acompanhamento.hide();
+					bebidas.hide();
 					break;
 				case "Petiscos":
-					principal.collapse();
-					carnes.collapse();
-					petiscos.expand();
-					bolinhos.expand();
-					sobremesa.collapse();
-					doses.collapse();
-					chops.collapse();
-					bebidas.collapse();
-					acompanhamento.collapse();
+					petiscos.show();
+					carnes.hide();
+					principal.hide();
+					sobremesa.hide();
+					acompanhamento.hide();
+					bebidas.hide();
 					break;
 				case "Sobremesa":
-					principal.collapse();
-					carnes.collapse();
-					petiscos.collapse();
-					bolinhos.collapse();
-					sobremesa.expand();
-					doses.collapse();
-					chops.collapse();
-					bebidas.collapse();
-					acompanhamento.collapse();
+					petiscos.hide();
+					carnes.hide();
+					principal.hide();
+					sobremesa.show();
+					acompanhamento.hide();
+					bebidas.hide();
 					break;
 				case "Bebidas":
-					principal.collapse();
-					carnes.collapse();
-					petiscos.collapse();
-					bolinhos.collapse();
-					sobremesa.collapse();
-					doses.expand();
-					chops.expand();
-					bebidas.expand();
-					acompanhamento.collapse();
+					petiscos.hide();
+					carnes.hide();
+					principal.hide();
+					sobremesa.hide();
+					acompanhamento.hide();
+					bebidas.show();
 					break;
 				case "Acompanhamentos":
-					principal.collapse();
-					carnes.collapse();
-					petiscos.collapse();
-					bolinhos.collapse();
-					sobremesa.collapse();
-					doses.collapse();
-					chops.collapse();
-					bebidas.collapse();
-					acompanhamento.expand();
+					petiscos.hide();
+					carnes.hide();
+					principal.hide();
+					sobremesa.hide();
+					acompanhamento.show();
+					bebidas.hide();
 					break;
 				default:
 					principal.expand();
-				break;
+					break;
 			}
 		})
 	}
